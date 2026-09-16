53af2418204be33b6d0a8890cbb296d005a18e01
squidyxxx34-prog <squidyxxx34@gmail.com>
2026-09-16 23:08:03 +0200

    Initialize Discord bot client with login

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Bot connecté : ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
